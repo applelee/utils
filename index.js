38fe5a9dc3ea8fd857230a949b1f9d62149c6e41
李佳 <928904929@qq.com>
2020-07-31 17:05:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+import {
+  PHONE_REGEX,
+  EMAIL_REGEX,
+} from './constants';
+
+/**
+ * 
+ * @param {function} generator 函数生成器
+ * @returns {void}
+ */
 export const Co = generator => {
-  const ge = generator()
-  
+  const ge = generator();
   const geLoop = next => {
-    !next.done && next.value.then(res => geLoop(ge.next(res)))
-  }
+    !next.done && next.value.then(res => geLoop(ge.next(res)));
+  };
   
-  geLoop(ge.next())
-}
+  geLoop(ge.next());
+};
+
+/**
+ * 
+ * @param {number} phone 电话号码
+ * @returns {boolean} 电话号码格式是否合法
+ */
+export const isPhone = phone => PHONE_REGEX.test(phone);
+
+/**
+ * 
+ * @param {string} email 电子邮箱
+ * @returns {boolean} 电子邮箱格式是否合法
+ */
+export const isEmail = email => EMAIL_REGEX.test(email);
