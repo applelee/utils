0e49cb34d9f06567f6ec619a03f4f5c2d4040a1f
李佳 <928904929@qq.com>
2020-07-31 17:05:56 +0800

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const APP_JSON = 'application/json';
+export const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+export const FORM_DATA = 'multipart/form-data';
+
+export const PHONE_REGEX = /^1([3-9])\d{9}$/;
+export const EMAIL_REGEX = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
