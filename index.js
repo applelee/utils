16673b0f4aab1f0444a34f6e152dcbd0aa2f57fb
李佳 <928904929@qq.com>
2020-09-01 15:42:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -272,3 +272,63 @@ const matirx2dRotation = (matrix2d, clockwise = true) => {
   };
   return newMatirx2d;
 };
+
+/**
+ * 
+ * 绕Z轴2d矩阵
+ * @param {number} angle 角度
+ * @param {boolean} clockwise 是否顺时针
+ * @returns {array} 2d矩阵（2维数组）
+ */
+const matrix2dZ = (angle, clockwise = true) => {
+  const radian = angle * Math.PI / 180;
+  const cos = () => Math.abs(angle % 180) === 90 ? 0 : Math.cos(radian);
+  const sin = () => Math.sin(radian);
+  
+  return [
+    [cos(), clockwise ? -sin() : sin(), 0],
+    [clockwise ? sin() : -sin(), cos(), 0],
+    [0, 0, 1],
+  ];
+};
+
+/**
+ * 
+ * 点积运算
+ * @param {array} v1 矢量
+ * @param {array} v2 矢量
+ * @returns {number} 点积 
+ */
+const dotProduct = (v1, v2) => {
+  let num = v1.length;
+
+  const loopFn = (dot = 0) => {
+    num --;
+    if (num < 0) return dot;
+    return loopFn(dot + v1[num] * v2[num]);
+  };
+  return loopFn();
+};
+
+/**
+ * 
+ * 矢量旋转
+ * @param {array} vector 矢量
+ * @param {array} maxtix2d 2d矩阵
+ * @returns {array} 旋转后的矢量矢量
+ */
+const vectorRotation = (vector, maxtix2d) => {
+  const row = maxtix2d.length;
+  const col = maxtix2d[0].length;
+  const newVector = [];
+
+  for (let j = 0; j < col; j ++) {
+    const tampVector = [];
+    for (let k = 0; k < row; k ++) {
+      tampVector.push(maxtix2d[k][j]);
+    }
+    newVector.push(dotProduct(vector, tampVector));
+  }
+
+  return newVector;
+};
