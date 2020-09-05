e42482d013b58953889a0a4198e59fcd52c62475
lijia <41421669@qq.com>
2020-09-06 03:19:36 +0800

    升降序

--- libs/BinaryTree.js
+++ libs/BinaryTree.js
@@ -1,6 +1,12 @@
 (function (w) {
+  // 树总集
   let rootTree;
 
+  const binaryTree = (array = []) => {
+    return new BinaryTree(array);
+  }
+
+  // 空树构造函数
   const Tree = function () {
     // this.value;
     // this.leftTree;
@@ -8,47 +14,69 @@
   }
 
   /**
+   * 
+   * 二叉树构造函数
    * @param {array} array
    */
   const BinaryTree = function (array) {
     init(array);
   }
 
-  /**
-   * @param {array} array
-   */
-  BinaryTree.create = function (array) {
-    return new BinaryTree(array);
-  }
-
   BinaryTree.prototype = {
+    // 树总集
     get rootTree () {
       return rootTree;
     }
   }
   
+  // 插值
   BinaryTree.prototype.add = function (value) {
     addValue(value);
   }
 
+  // 查询
   BinaryTree.prototype.has = function (value) {
     return hasValue(value);
   } 
 
+  // 最小值
   BinaryTree.prototype.min = function () {
     return minTree().value;
   }
 
+  // 最大值
   BinaryTree.prototype.max = function () {
     return maxTree().value;
   }
 
+  // 删除
   BinaryTree.prototype.delete = function (value) {
     return deleteValue(value);
   }
 
+  // 清空
   BinaryTree.prototype.clear = function () {
     rootTree = new Tree();
+  }
+
+  // 前序
+  BinaryTree.prototype.preorderTraversal = function () {
+    return preorderTraversalRecursion();
+  }
+
+  // 中序（升序）
+  BinaryTree.prototype.inorderTraversalAsce = function () {
+    return inorderTraversalAsceRecursion();
+  }
+
+  // 中序（降序）
+  BinaryTree.prototype.inorderTraversalDesce = function () {
+    return inorderTraversalDesceRecursion();
+  }
+
+  // 后序
+  BinaryTree.prototype.postorderTraversal = function () {
+    return postorderTraversalRecursion();
   }
 
   const init = (array = []) => {
@@ -117,5 +145,37 @@
     return promotionTree(tempTree);
   }
 
-  w.BinaryTree = BinaryTree;
+  // 前序遍历
+  const preorderTraversalRecursion = (tree = rootTree, result = []) => {
+    if (tree.value !== undefined) result.push(tree.value);
+    if (tree.leftTree) preorderTraversalRecursion(tree.leftTree, result);
+    if (tree.rightTree) preorderTraversalRecursion(tree.rightTree, result);
+    return result;
+  }
+
+  // 中序遍历（升序）
+  const inorderTraversalAsceRecursion = (tree = rootTree, result = []) => {
+    if (tree.leftTree) inorderTraversalAsceRecursion(tree.leftTree, result);
+    if (tree.value !== undefined) result.push(tree.value);
+    if (tree.rightTree) inorderTraversalAsceRecursion(tree.rightTree, result);
+    return result
+  }
+
+  // 中序遍历（降序）
+  const inorderTraversalDesceRecursion = (tree = rootTree, result = []) => {
+    if (tree.rightTree) inorderTraversalDesceRecursion(tree.rightTree, result);
+    if (tree.value !== undefined) result.push(tree.value);
+    if (tree.leftTree) inorderTraversalDesceRecursion(tree.leftTree, result);
+    return result
+  }
+
+  // 后序遍历
+  const postorderTraversalRecursion = (tree = rootTree, result = []) => {
+    if (tree.leftTree) postorderTraversalRecursion(tree.leftTree, result);
+    if (tree.rightTree) postorderTraversalRecursion(tree.rightTree, result);
+    if (tree.value !== undefined) result.push(tree.value);
+    return result
+  }
+
+  w.bt = w.binarytree = binaryTree;
 })(window);
